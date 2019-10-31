4879accb08c22b86035e2b198e4c83d572d20b30
Jeremy Bayle <jeremy.bayle@drivequant.com>
2019-10-31 16:42:24 +0100

    Clean code

--- DriveKitApp/AppDelegate.swift
+++ DriveKitApp/AppDelegate.swift
@@ -75,7 +75,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         DriveKitTripAnalysis.shared.initialize(tripListener: self, appLaunchOptions: launchOptions)
         DriveKitDriverData.shared.initialize()
         if !DriveKit.shared.isConfigured() {
-            DriveKit.shared.setApiKey(key: "qDcgo5W2I1p3u5STEhuQ1AJo")
+            DriveKit.shared.setApiKey(key: "your API key here")
             DriveKitLog.shared.infoLog(tag: AppDelegate.tag, message: "DriveKit configured with API key")
         }
         if SettingsBundleKeys.getDefaultValuePref() {

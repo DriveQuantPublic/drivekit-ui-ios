fdd9fd67931fa87a2345abbd98db1c5bc7ca7677
Jeremy Bayle <jeremy.bayle@drivequant.com>
2020-04-09 13:43:47 +0200

    Update module deploy script

--- pushPrivatePodsModuleUI.sh
+++ pushPrivatePodsModuleUI.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pod repo push drivekit-specs DriveKitDriverDataUI.podspec --allow-warnings
-pod repo push drivekit-specs DriveKitDriverAchievementUI.podspec --allow-warnings
-pod repo push drivekit-specs DriveKitVehicleUI.podspec --allow-warnings
+pod repo push drivekit-specs DriveKitDriverDataUI.podspec --allow-warnings --sources=https://gitlab.com/drivequant/drivekit/drivekit-specs.git
+pod repo push drivekit-specs DriveKitDriverAchievementUI.podspec --allow-warnings --sources=https://gitlab.com/drivequant/drivekit/drivekit-specs.git
+pod repo push drivekit-specs DriveKitVehicleUI.podspec --allow-warnings --sources=https://gitlab.com/drivequant/drivekit/drivekit-specs.git

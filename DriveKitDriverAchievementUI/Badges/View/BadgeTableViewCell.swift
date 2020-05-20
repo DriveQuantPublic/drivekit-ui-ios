42ad2015529a817cd16f84ba9e25666c3d8d5d76
Fanny Tavart <fanny.tavart@drivequant.com>
2020-05-20 13:32:45 +0200

    fixed table view cell

--- DriveKitDriverAchievementUI/Badges/View/BadgeTableViewCell.swift
+++ DriveKitDriverAchievementUI/Badges/View/BadgeTableViewCell.swift
@@ -21,14 +21,23 @@ class BadgeTableViewCell : UITableViewCell {
     
     private func addLevelView(level: DKBadgeLevel) {
         let levelView = BadgeLevelView.viewFromNib
-        levelView.configure(level: level)
         levelsStackView.addArrangedSubview(levelView)
+        configureLevelView(levelView: levelView, level: level)
+    }
+
+    private func configureLevelView(levelView: BadgeLevelView, level: DKBadgeLevel) {
+        levelView.configure(level: level)
     }
 
     func configure(levels: [DKBadgeLevel]) {
-        levelsStackView.removeAllSubviews()
-        addLevelView(level: levels.filter{$0.level == .bronze}.first!)
-        addLevelView(level: levels.filter{$0.level == .silver}.first!)
-        addLevelView(level: levels.filter{$0.level == .gold}.first!)
+        if levelsStackView.arrangedSubviews.isEmpty {
+            addLevelView(level: levels.filter{$0.level == .bronze}.first!)
+            addLevelView(level: levels.filter{$0.level == .silver}.first!)
+            addLevelView(level: levels.filter{$0.level == .gold}.first!)
+        } else {
+            configureLevelView(levelView: levelsStackView.arrangedSubviews[0] as! BadgeLevelView, level: levels.filter{$0.level == .bronze}.first!)
+            configureLevelView(levelView: levelsStackView.arrangedSubviews[0] as! BadgeLevelView, level: levels.filter{$0.level == .silver}.first!)
+            configureLevelView(levelView: levelsStackView.arrangedSubviews[0] as! BadgeLevelView, level: levels.filter{$0.level == .gold}.first!)
+        }
     }
 }

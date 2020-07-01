9a65fa758a6e7e1c878e000d95f90a3375bcef96
David Bauduin <david.bauduin@drivequant.com>
2020-07-01 10:53:20 +0200

    Fix configuration of badgeCategories and make DriveKitDriverAchievementUI accessible from Objective-C (#26)

--- DriveKitDriverAchievementUI/DriveKitDriverAchievementUI.swift
+++ DriveKitDriverAchievementUI/DriveKitDriverAchievementUI.swift
@@ -10,29 +10,29 @@ import UIKit
 import DriveKitCommonUI
 import DriveKitDBAchievementAccess
 
-public class DriveKitDriverAchievementUI {
-    
-    public static let shared = DriveKitDriverAchievementUI()
-    
-    var streakThemes : [DKStreakTheme] = [.phoneDistraction, .safety, .acceleration, .brake, .adherence]
-    var badgeCategories : [DKBadgeCategory] = [.generic, .ecodriving, .safety, .phoneDistraction]
-    
-    private init() {}
-    
-    public func initialize() {
+@objc public class DriveKitDriverAchievementUI : NSObject {
+
+    @objc public static let shared = DriveKitDriverAchievementUI()
+
+    public private(set) var streakThemes: [DKStreakTheme] = [.phoneDistraction, .safety, .acceleration, .brake, .adherence]
+    public private(set) var badgeCategories: [DKBadgeCategory] = [.generic, .ecodriving, .safety, .phoneDistraction]
+
+    private override init() {}
+
+    @objc public func initialize() {
         DriveKitNavigationController.shared.driverAchievementUI = self
     }
-    
-    public func configureStreakThemes(streakThemes : [DKStreakTheme]) {
+
+    public func configureStreakThemes(streakThemes: [DKStreakTheme]) {
         self.streakThemes = streakThemes
     }
-    
-    public func configureBadgeCategories(badgeCategories : [DKBadgeCategory]) {
-        if !badgeCategories.contains(.generic) {
-            self.badgeCategories = [.generic]
-        }
-        self.badgeCategories.append(contentsOf: badgeCategories)
+
+    public func configureBadgeCategories(badgeCategories: [DKBadgeCategory]) {
+        var newBadgeCategories: [DKBadgeCategory] = (badgeCategories.contains(.generic) ? [] : [.generic])
+        newBadgeCategories.append(contentsOf: badgeCategories)
+        self.badgeCategories = newBadgeCategories
     }
+
 }
 
 extension Bundle {
@@ -49,8 +49,24 @@ extension DriveKitDriverAchievementUI : DriveKitDriverAchievementUIEntryPoint {
     public func getStreakViewController() -> UIViewController {
         return StreakViewController()
     }
-    
+
     public func getBadgesViewController() -> UIViewController {
         return BadgesViewController()
     }
+}
+
+// MARK: - Objective-C extension
+
+extension DriveKitDriverAchievementUI {
+
+    @objc(configureStreakThemes:) // Usage example: [DriveKitDriverAchievementUI.shared configureStreakThemes:@[ @(DKStreakThemePhoneDistraction), @(DKStreakThemeSafety), @(DKStreakThemeAcceleration), @(DKStreakThemeBrake), @(DKStreakThemeAdherence) ]];
+    public func objc_configureStreakThemes(streakThemes: [Int]) {
+        configureStreakThemes(streakThemes: streakThemes.map { DKStreakTheme(rawValue: $0)! })
+    }
+
+    @objc(configureBadgeCategories:) // Usage example: [DriveKitDriverAchievementUI.shared configureBadgeCategories:@[ @(DKBadgeCategoryGeneric), @(DKBadgeCategoryEcodriving), @(DKBadgeCategorySafety), @(DKBadgeCategoryPhoneDistraction) ]];
+    public func objc_configureBadgeCategories(badgeCategories: [Int]) {
+        configureBadgeCategories(badgeCategories: badgeCategories.map { DKBadgeCategory(rawValue: $0)! })
+    }
+
 }

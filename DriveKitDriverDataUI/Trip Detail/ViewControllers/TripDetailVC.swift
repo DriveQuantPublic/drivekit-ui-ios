DriveQuantPublic/drivekit-ui-ios

commit a5bab81326ba0425c1417299bfe34a4b3bc7cf6e

Fix bug with map item selection

DriveKitDriverDataUI/Trip Detail/ViewControllers/TripDetailVC.swift
@@ -337,7 +337,15 @@ extension TripDetailVC: TripDetailDelegate {
 
 extension TripDetailVC: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
-        guard let viewControllerIndex = swipableViewControllers.firstIndex(of: pendingViewControllers[0]) else {
+        updateState(from: pendingViewControllers)
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        updateState(from: previousViewControllers)
+    }
+
+    private func updateState(from viewControllers: [UIViewController]) {
+        guard let viewControllerIndex = swipableViewControllers.firstIndex(of: viewControllers[0]) else {
             return
         }
         let mapItem = self.viewModel.configurableMapItems[viewControllerIndex]

commit 552a65c10fe15a3c40c54f6afe1d9399fd2943e4

Fix bug introduced in commit a5bab81326ba0425c1417299bfe34a4b3bc7cf6e

DriveKitDriverDataUI/Trip Detail/ViewControllers/TripDetailVC.swift
@@ -341,7 +341,9 @@ extension TripDetailVC: UIPageViewControllerDelegate {
     }
 
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        updateState(from: previousViewControllers)
+        if !completed {
+            updateState(from: previousViewControllers)
+        }
     }
 
     private func updateState(from viewControllers: [UIViewController]) {
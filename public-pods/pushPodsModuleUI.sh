700bc47f199d786aed68f70a0ce08226a6392aeb
David Bauduin <david.bauduin@drivequant.com>
2021-03-11 18:03:51 +0100

    Try to fix this Cocoapods issue: https://github.com/CocoaPods/CocoaPods/issues/9497 when pushing modules (#62)

--- public-pods/pushPodsModuleUI.sh
+++ public-pods/pushPodsModuleUI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pod trunk push DriveKitDriverDataUI.podspec
-pod trunk push DriveKitDriverAchievementUI.podspec --allow-warnings
-pod trunk push DriveKitVehicleUI.podspec --allow-warnings
-pod trunk push DriveKitPermissionsUtilsUI.podspec
+pod trunk push DriveKitDriverDataUI.podspec --synchronous
+pod trunk push DriveKitDriverAchievementUI.podspec --allow-warnings --synchronous
+pod trunk push DriveKitVehicleUI.podspec --allow-warnings --synchronous
+pod trunk push DriveKitPermissionsUtilsUI.podspec --synchronous

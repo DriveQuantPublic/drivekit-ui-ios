2155b37ede5c4cb6e401bd4812c03d5ba57d6dda
David Bauduin <david.bauduin@drivequant.com>
2022-03-18 09:03:09 +0100

    Add localized keys for crash feedback notification

--- DriveKitApp/AppDelegate.swift
+++ DriveKitApp/AppDelegate.swift
@@ -130,7 +130,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
         DriveKitTripAnalysis.shared.activateCrashDetection(true)
-        let crashFeedbackConfig = DKCrashFeedbackConfig(notification: DKCrashFeedbackNotification(title: "Collision detected!", message: "Tap to get help / to provide feedback", crashAlert: .vibration))
+        let crashFeedbackConfig = DKCrashFeedbackConfig(notification: DKCrashFeedbackNotification(title: "dk_crash_detection_feedback_notif_title".dkTripAnalysisLocalized(), message: "dk_crash_detection_feedback_notif_message".dkTripAnalysisLocalized(), crashAlert: .vibration))
         DriveKitTripAnalysis.shared.enableCrashFeedback(config: crashFeedbackConfig)
     }
 }

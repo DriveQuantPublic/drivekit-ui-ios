292d157005678096593533360a25c899a633d181
David Bauduin <david.bauduin@drivequant.com>
2022-04-12 17:55:50 +0200

    Update pods

--- DriveKitCommonUI/Component/TripsList/DKTripList.swift
+++ DriveKitCommonUI/Component/TripsList/DKTripList.swift
@@ -43,7 +43,7 @@ public protocol DKTripList: AnyObject {
 
 public extension DKTripListItem {
     var computedDepartureInfo: String {
-        if let departureCity = getDepartureCity(), !departureCity.isCompletelyEmpty() && departureCity != DKAddress.unknownValue {
+        if let departureCity = getDepartureCity(), !departureCity.isCompletelyEmpty() && departureCity != "N/A" {
             return departureCity
         } else {
             return getDepartureAddress() ?? ""
@@ -51,7 +51,7 @@ public extension DKTripListItem {
     }
 
     var computedArrivalInfo: String {
-        if let arrivalCity = getArrivalCity(), !arrivalCity.isCompletelyEmpty() && arrivalCity != DKAddress.unknownValue {
+        if let arrivalCity = getArrivalCity(), !arrivalCity.isCompletelyEmpty() && arrivalCity != "N/A" {
             return arrivalCity
         } else {
             return getArrivalAddress() ?? ""

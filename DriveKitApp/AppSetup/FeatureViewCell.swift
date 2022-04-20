664aa4f2c7e036c141846e9f89f3ba1ffbb9fa6a
David Bauduin <david.bauduin@drivequant.com>
2022-04-20 09:14:59 +0200

    Add feature list screen

---
+++ DriveKitApp/AppSetup/FeatureViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  FeatureViewCell.swift
+//  DriveKitApp
+//
+//  Created by David Bauduin on 18/04/2022.
+//  Copyright © 2022 DriveQuant. All rights reserved.
+//
+
+import UIKit
+import DriveKitCommonUI
+
+final class FeatureViewCell: UITableViewCell, Nibable {
+    private let featureView: FeatureView = FeatureView.viewFromNib
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        self.featureView.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(self.featureView)
+        self.featureView.addShadow()
+        let verticalMargin: CGFloat = 10
+        let horizontalMargin: CGFloat = 8
+        NSLayoutConstraint.activate([
+            self.featureView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: verticalMargin),
+            self.featureView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -verticalMargin),
+            self.featureView.leftAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: horizontalMargin),
+            self.featureView.rightAnchor.constraint(equalTo: self.contentView.rightAnchor, constant: -horizontalMargin)
+        ])
+    }
+
+    func update(with viewModel: FeatureViewViewModel, parentViewController: UIViewController) {
+        self.featureView.update(viewModel: viewModel, parentViewController: parentViewController)
+    }
+}

33a40282a60981d35b1299e13f10fcbf81d91146
David Bauduin <david.bauduin@drivequant.com>
2022-10-18 16:19:22 +0200

    [WIP] Manage Timeline screen

--- DriveKitDriverDataTimelineUI/Timeline/TimelineViewController.swift
+++ DriveKitDriverDataTimelineUI/Timeline/TimelineViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import DriveKitCommonUI
 
 class TimelineViewController: UIViewController {
     @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var scoreSelectorView: UIStackView!
+    private let viewModel: TimelineViewModel
+    private var selectedScoreSelectionTypeView: ScoreSelectionTypeView? = nil
 
-    init() {
+    init(viewModel: TimelineViewModel) {
+        self.viewModel = viewModel
         super.init(nibName: String(describing: TimelineViewController.self), bundle: .driverDataTimelineUIBundle)
+        viewModel.delegate = self
     }
 
     required init?(coder: NSCoder) {
@@ -25,9 +31,63 @@ class TimelineViewController: UIViewController {
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refresh(_ :)), for: .valueChanged)
         self.scrollView.refreshControl = refreshControl
+
+        let scores = self.viewModel.scores
+        if scores.count < 2 {
+            self.scoreSelectorView.isHidden = true
+        } else {
+            let selectedRankingType = self.viewModel.selectedScore
+            self.scoreSelectorView.isHidden = false
+            self.scoreSelectorView.removeAllSubviews()
+            for score in scores {
+                if let selectorView = Bundle.driverDataTimelineUIBundle?.loadNibNamed("ScoreSelectionTypeView", owner: nil, options: nil)?.first as? ScoreSelectionTypeView {
+                    selectorView.update(scoreType: score)
+                    let selected = score == self.viewModel.selectedScore
+                    selectorView.setSelected(selected)
+                    if selected {
+                        self.selectedScoreSelectionTypeView = selectorView
+                    }
+                    selectorView.addTarget(self, action: #selector(onScoreSelectionTypeViewSelected(sender:)), for: .touchUpInside)
+                    self.scoreSelectorView.addArrangedSubview(selectorView)
+                }
+            }
+        }
+
+        if self.viewModel.updating {
+            showRefreshControl()
+        } else {
+            hideRefreshControl()
+        }
     }
 
     @objc private func refresh(_ sender: Any) {
+        self.viewModel.updateTimeline()
+    }
 
+    private func showRefreshControl() {
+        self.scrollView.refreshControl?.beginRefreshing()
+    }
+
+    private func hideRefreshControl() {
+        self.scrollView.refreshControl?.endRefreshing()
+    }
+
+    @objc private func onScoreSelectionTypeViewSelected(sender: ScoreSelectionTypeView) {
+        self.selectedScoreSelectionTypeView?.setSelected(false)
+        self.selectedScoreSelectionTypeView = sender
+        sender.setSelected(true)
+        if let scoreType = sender.scoreType, self.viewModel.selectedScore != scoreType {
+            self.viewModel.selectedScore = scoreType
+        }
+    }
+}
+
+extension TimelineViewController: TimelineViewModelDelegate {
+    func willUpdateTimeline() {
+        showRefreshControl()
+    }
+
+    func didUpdateTimeline() {
+        hideRefreshControl()
     }
 }
--- DriveKitDriverDataTimelineUI/Timeline/TimelineViewModel.swift
+++ DriveKitDriverDataTimelineUI/Timeline/TimelineViewModel.swift
@@ -7,7 +7,92 @@
 //
 
 import Foundation
+import DriveKitCommonUI
+import DriveKitDBTripAccessModule
+import DriveKitDriverDataModule
 
 class TimelineViewModel {
-    
+    private(set) var updating: Bool = false
+    weak var delegate: TimelineViewModelDelegate?
+    let scores: [DKTimelineScoreType]
+    let dateSelectorViewModel: DateSelectorViewModel
+    let timelineGraphViewModel: TimelineGraphViewModel
+    let periodSelectorViewModel: PeriodSelectorViewModel
+    let roadContextViewModel: RoadContextViewModel
+    var selectedScore: DKTimelineScoreType {
+        didSet {
+            updateUI()
+        }
+    }
+    private var weekTimeline: DKTimeline?
+    private var monthTimeline: DKTimeline?
+
+    init() {
+        self.scores = DriveKitDriverDataTimelineUI.shared.scores
+        self.selectedScore = self.scores.first ?? .safety
+
+        self.dateSelectorViewModel = DateSelectorViewModel()
+        self.timelineGraphViewModel = TimelineGraphViewModel()
+        self.periodSelectorViewModel = PeriodSelectorViewModel()
+        self.roadContextViewModel = RoadContextViewModel()
+
+        DriveKitDriverData.shared.getTimelines(periods: [.week, .month], type: .cache) { [weak self] status, timelines in
+            if let self {
+                if status == .cacheDataOnly, let timelines {
+                    for timeline in timelines {
+                        switch timeline.period {
+                            case .month:
+                                self.monthTimeline = timeline
+                            case .week:
+                                self.weekTimeline = timeline
+                        }
+                    }
+                }
+            }
+        }
+        updateTimeline()
+    }
+
+    func updateTimeline() {
+        self.updating = true
+        self.delegate?.willUpdateTimeline()
+        DriveKitDriverData.shared.getTimelines(periods: [.week, .month], type: .defaultSync) { [weak self] status, timelines in
+            if let self {
+                if status != .noTimelineYet, let timelines {
+                    for timeline in timelines {
+                        switch timeline.period {
+                            case .month:
+                                self.monthTimeline = timeline
+                            case .week:
+                                self.weekTimeline = timeline
+                        }
+                    }
+                }
+                self.updating = false
+                self.delegate?.didUpdateTimeline()
+            }
+        }
+    }
+
+    private func updateUI() {
+
+    }
+}
+
+extension TimelineViewModel: PeriodSelectorDelegate {
+    func periodSelectorDidSelectPeriod(_ period: TimelinePeriod) {
+        //TODO
+    }
+}
+
+extension TimelineViewModel: DateSelectorDelegate {
+    func dateSelectorDidSelectDate(_ date: Date) {
+        //TODO
+    }
+}
+
+extension TimelineViewModel: TimelineGraphDelegate {
+    func graphDidSelectDate(_ date: Date) {
+        //TODO
+    }
 }

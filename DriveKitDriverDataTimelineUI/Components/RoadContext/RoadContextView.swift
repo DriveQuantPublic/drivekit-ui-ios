5b8a0ee2854d0b614304886a40d10fd4cb8eec73
Amine GAHBICHE <amine.gahbiche@drivequant.com>
2022-10-24 13:53:32 +0100

    continue implementing RoadContext timeline component (view and viewmodel implementation)

--- DriveKitDriverDataTimelineUI/Components/RoadContext/RoadContextView.swift
+++ DriveKitDriverDataTimelineUI/Components/RoadContext/RoadContextView.swift
@@ -7,7 +7,61 @@
 //
 
 import UIKit
+import DriveKitDBTripAccessModule
 
-class RoadContextView {
-    
+class RoadContextView: UIView {
+    static private let radius: Double = 10
+    static private let margin: Double = 5
+    private var viewModel: RoadContextViewModel?
+
+    override func draw(_ rect: CGRect) {
+        guard let itemsToDraw = viewModel?.itemsToDraw, itemsToDraw.count > 0 else {
+            return
+        }
+        var drawnPercent: Double = 0
+        for i in 0..<itemsToDraw.count {
+            let roundedStart: Bool = (i == 0)
+            let roundedEnd: Bool = (i == itemsToDraw.count - 1)
+            let startX = drawnPercent*(rect.width - 2*RoadContextView.margin) + RoadContextView.margin
+            let itemRect = CGRect(x: startX, y: RoadContextView.margin, width: (rect.width - 2*RoadContextView.margin)*itemsToDraw[i].percent, height: rect.height - 2*RoadContextView.margin)
+            self.drawPartView(itemRect, color: RoadContextViewModel.getContextColor(itemsToDraw[i].context).cgColor, roundedStart: roundedStart, roundedEnd: roundedEnd)
+            drawnPercent = drawnPercent + itemsToDraw[i].percent
+        }
+    }
+
+    private func drawPartView(_ rect: CGRect, color: CGColor, roundedStart: Bool = false, roundedEnd: Bool = false) {
+        let bezierPath = UIBezierPath()
+        let startPoint = rect.origin
+        let endPoint = CGPoint(x: rect.origin.x + rect.width, y: rect.origin.y + rect.height)
+        if roundedStart {
+            bezierPath.addArc(withCenter: CGPoint(x: startPoint.x + RoadContextView.radius, y: startPoint.y + RoadContextView.radius), radius: RoadContextView.radius, startAngle: .pi, endAngle: .pi*3/2, clockwise: true)
+        } else {
+            bezierPath.move(to: startPoint)
+        }
+        if roundedEnd {
+            bezierPath.addLine(to: CGPoint(x: endPoint.x - RoadContextView.radius, y: startPoint.y))
+            bezierPath.addArc(withCenter: CGPoint(x: endPoint.x - RoadContextView.radius, y: startPoint.y + RoadContextView.radius), radius: RoadContextView.radius, startAngle: .pi*3/2, endAngle: .pi*2, clockwise: true)
+            bezierPath.addLine(to: CGPoint(x: endPoint.x, y: endPoint.y - RoadContextView.radius))
+            bezierPath.addArc(withCenter: CGPoint(x: endPoint.x - RoadContextView.radius, y: endPoint.y - RoadContextView.radius), radius: RoadContextView.radius, startAngle: 0, endAngle: .pi/2, clockwise: true)
+        } else {
+            bezierPath.addLine(to: CGPoint(x: endPoint.x, y: startPoint.y))
+            bezierPath.addLine(to: CGPoint(x: endPoint.x, y: endPoint.y))
+        }
+        if roundedStart {
+            bezierPath.addLine(to: CGPoint(x: startPoint.x + RoadContextView.radius, y: endPoint.y))
+            bezierPath.addLine(to: CGPoint(x: startPoint.x + RoadContextView.radius, y: endPoint.y))
+            bezierPath.addArc(withCenter: CGPoint(x: startPoint.x + RoadContextView.radius, y: endPoint.y - RoadContextView.radius), radius: RoadContextView.radius, startAngle: .pi/2, endAngle: .pi, clockwise: true)
+            bezierPath.addLine(to: CGPoint(x: startPoint.x, y: startPoint.y - RoadContextView.radius))
+        } else {
+            bezierPath.addLine(to: CGPoint(x: startPoint.x, y: endPoint.y))
+            bezierPath.addLine(to: CGPoint(x: startPoint.x, y: startPoint.y))
+        }
+        UIGraphicsGetCurrentContext()?.setFillColor(color)
+        bezierPath.fill()
+    }
+
+    func configure(viewModel: RoadContextViewModel) {
+        self.viewModel = viewModel
+        self.setNeedsDisplay()
+    }
 }
--- DriveKitDriverDataTimelineUI/Components/RoadContext/RoadContextViewModel.swift
+++ DriveKitDriverDataTimelineUI/Components/RoadContext/RoadContextViewModel.swift
@@ -12,11 +12,37 @@ import DriveKitDBTripAccessModule
 class RoadContextViewModel {
     private let distanceByContext: [DKRoadContext: Double]
     private let distance: Double
+    private static let backgroundColor = UIColor(hex: 0xFAFAFA)
+    private static let urbainDenseColor = UIColor(hex: 0x036A82)
+    private static let subUrbainColor = UIColor(hex: 0x699DAD)
+    private static let urbainFluidColor = UIColor(hex: 0x3B8497)
+    private static let highwayColor = UIColor(hex: 0x8FB7C2)
 
-    init(distanceByContext: [DKRoadContext : Double], distance: Double) {
+    init(distanceByContext: [DKRoadContext: Double], distance: Double) {
         self.distanceByContext = distanceByContext
         self.distance = distance
     }
+
+    lazy var itemsToDraw: [(context: DKRoadContext, percent: Double)] = {
+        var result: [(context: DKRoadContext, percent: Double)] = []
+        let heavyUrbanTrafficPercent = getHeavyUrbanTrafficPercent()
+        if heavyUrbanTrafficPercent > 0 {
+            result.append((.heavyUrbanTraffic, heavyUrbanTrafficPercent))
+        }
+        let cityPercent = getCityPercent()
+        if cityPercent > 0 {
+            result.append((.city, cityPercent))
+        }
+        let suburbanPercent = getSuburbanPercent()
+        if suburbanPercent > 0 {
+            result.append((.suburban, suburbanPercent))
+        }
+        let expresswaysPercent = getExpresswaysPercent()
+        if expresswaysPercent > 0 {
+            result.append((.expressways, expresswaysPercent))
+        }
+       return result
+    }()
 
     func getTitle() -> String {
         return "dk_road_context_title".dkDriverDataTimelineLocalized() + " (\(distance.formatMeterDistanceInKm())"
@@ -68,5 +94,20 @@ class RoadContextViewModel {
             return 0
         }
         return distance/totalCalculatedDistance
+    }
+
+    static func getContextColor(_ context: DKRoadContext) -> UIColor {
+        switch context {
+        case .suburban:
+            return RoadContextViewModel.subUrbainColor
+        case .expressways:
+            return RoadContextViewModel.highwayColor
+        case .heavyUrbanTraffic:
+            return RoadContextViewModel.urbainDenseColor
+        case .city:
+            return RoadContextViewModel.urbainFluidColor
+        default:
+            return RoadContextViewModel.backgroundColor
+        }
     }
 }

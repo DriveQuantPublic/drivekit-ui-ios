ad58eaa847c9bee6165ea61621fba1b396b653e2
Frédéric Ruaudel <github@fredericruaudel.fr>
2022-12-13 10:28:10 +0100

    define associated detailed score with main score

--- DriveKitDriverDataTimelineUI/DKTimelineScoreType.swift
+++ DriveKitDriverDataTimelineUI/DKTimelineScoreType.swift
@@ -40,4 +40,35 @@ extension DKTimelineScoreType {
                 return DriveKitAccess.shared.hasAccess(.speeding)
         }
     }
+    
+    var associatedScoreItemTypes: [TimelineScoreItemType] {
+        switch self {
+        case .safety:
+            return [
+                .safety_acceleration,
+                .safety_braking,
+                .safety_adherence
+            ]
+        case .ecoDriving:
+            return [
+                .ecoDriving_efficiencyAcceleration,
+                .ecoDriving_efficiencyBrake,
+                .ecoDriving_efficiencySpeedMaintain,
+                .ecoDriving_fuelVolume,
+                .ecoDriving_fuelSavings,
+                .ecoDriving_co2mass
+            ]
+        case .distraction:
+            return [
+                .distraction_unlock,
+                .distraction_callForbiddenDuration,
+                .distraction_percentageOfTripsWithForbiddenCall
+            ]
+        case .speeding:
+            return [
+                .speeding_duration,
+                .speeding_distance
+            ]
+        }
+    }
 }

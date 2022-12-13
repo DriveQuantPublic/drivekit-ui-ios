60322aba644a345e59414823f0286677943a749c
Frédéric Ruaudel <github@fredericruaudel.fr>
2022-12-13 08:45:53 +0100

    Add two timeline detail delegate classes

---
+++ DriveKitDriverDataTimelineUI/TimelineDetail/TimelineDetailViewControllerDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  TimelineDetailViewControllerDelegate.swift
+//  DriveKitDriverDataTimelineUI
+//
+//  Created by Frédéric Ruaudel on 13/12/2022.
+//  Copyright © 2022 DriveQuant. All rights reserved.
+//
+
+import Foundation
+
+protocol TimelineDetailViewControllerDelegate: AnyObject {
+    
+}

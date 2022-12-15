3239ecb6ecc036f1e13efc880063db7a85255a9b
Frédéric Ruaudel <github@fredericruaudel.fr>
2022-12-15 14:56:44 +0100

    Move some road context calculation from timeline data in an DKTimeline extension

---
+++ DriveKitDriverDataTimelineUI/DKTimelineExtension.swift
@@ -0,0 +1,55 @@
+//
+//  DKTimelineExtension.swift
+//  DriveKitDriverDataTimelineUI
+//
+//  Created by Frédéric Ruaudel on 13/12/2022.
+//  Copyright © 2022 DriveQuant. All rights reserved.
+//
+import Foundation
+import DriveKitDBTripAccessModule
+
+extension DKTimeline {
+    /// Compute distance by road contexts from the timeline
+    func distanceByRoadContext(
+        selectedScore: DKTimelineScoreType,
+        selectedIndex: Int
+    ) -> [TimelineRoadContext: Double] {
+        var distanceByContext: [TimelineRoadContext: Double] = [:]
+        if self.hasValidTripScored(for: selectedScore, selectedIndex: selectedIndex) {
+            for roadContext in self.roadContexts {
+                if let timelineRoadContext = TimelineRoadContext(roadContext: roadContext.type) {
+                    let distance = roadContext.distance[selectedIndex]
+                    if distance > 0 {
+                        distanceByContext[timelineRoadContext] = distance
+                    }
+                }
+            }
+        }
+        
+        return distanceByContext
+    }
+
+    func totalDistanceForAllContexts(
+        selectedScore: DKTimelineScoreType,
+        selectedIndex: Int
+    ) -> Double {
+        var totalDistanceForAllContexts: Double = 0
+        if self.hasValidTripScored(for: selectedScore, selectedIndex: selectedIndex) {
+            totalDistanceForAllContexts = self.allContext.distance[selectedIndex]
+        }
+        
+        return totalDistanceForAllContexts
+    }
+    
+    private func hasValidTripScored(
+        for selectedScore: DKTimelineScoreType,
+        selectedIndex: Int
+    ) -> Bool {
+        // Distraction and Speeding can have score for short trip which
+        // are not counted in `numberTripScored`. `numberTripScored` only
+        // count fully scored trip for all four scores
+        return selectedScore == .distraction
+            || selectedScore == .speeding
+            || self.allContext.numberTripScored[selectedIndex] > 0
+    }
+}

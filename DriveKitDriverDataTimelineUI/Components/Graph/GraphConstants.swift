7c69de969ec18c49cf08eb1a708a349c38db5644
Frédéric Ruaudel <github@fredericruaudel.fr>
2022-12-23 15:30:42 +0100

    revert the correct graph line color

--- DriveKitDriverDataTimelineUI/Components/Graph/GraphConstants.swift
+++ DriveKitDriverDataTimelineUI/Components/Graph/GraphConstants.swift
@@ -10,7 +10,7 @@ import UIKit
 import DriveKitCommonUI
 
 enum GraphConstants {
-    static let defaultLineColor = UIColor(hex: 0x0B4D6E)
+    static let defaultLineColor = UIColor(hex: 0x083B54)
     static let defaultSelectedColor = DKUIColors.secondaryColor.color
     
     static let defaultNumberOfIntervalInYAxis = 10

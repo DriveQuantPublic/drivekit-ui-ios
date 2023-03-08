6221621c73381cda64d03e17e79ddd3fb88de177
Frédéric Ruaudel <frederic.ruaudel@drivequant.com>
2023-03-08 16:02:08 +0100

    Feature/447 score legend screen (#224)
    * create DKScoreTypeLevel model to define the levels of each score types
    
    * Split DKScoreTypeLevel to move all l10n management into DriverDataUI module and create a protocol to add upperBound and lowerBound to the type returned by scoreLevels method
    
    * Create ScoreLevelLegendViewController and its viewModel
    
    * Finish designing the screen
    
    * Temporarily display the legend screen via the detail button
    
    * update localisation strings
    
    * Uppercased the close button title to better match original design

---
+++ DriveKitDriverDataUI/Helpers/DKScoreTypeLevel+l10n.swift
@@ -0,0 +1,68 @@
+//
+//  DKScoreTypeLevel+l10n.swift
+//  DriveKitDriverDataUI
+//
+//  Created by Frédéric Ruaudel on 03/03/2023.
+//  Copyright © 2023 DriveQuant. All rights reserved.
+//
+
+import DriveKitCommonUI
+import DriveKitCoreModule
+import Foundation
+
+extension DKScoreTypeLevel {
+    public func localizedTitle(for scoreType: DKScoreType) -> String {
+        let scoreLevelRange = self.scoreLevels(for: scoreType)
+        return String(
+            format: localizedTitleKey.dkDriverDataLocalized(),
+            scoreLevelRange.lowerBound.formatDouble(places: 1),
+            scoreLevelRange.upperBound.formatDouble(places: 1)
+        )
+    }
+    
+    public func localizedDescription(for scoreType: DKScoreType) -> String {
+        localizedDescriptionKey(for: scoreType).dkDriverDataLocalized()
+    }
+    
+    private var localizedTitleKey: String {
+        "dk_driverdata_mysynthesis_score_title_" + self.localizedScoreLevelKeySuffix
+    }
+    
+    private func localizedDescriptionKey(for scoreType: DKScoreType) -> String {
+        "dk_driverdata_mysynthesis_\(scoreType.localizedScoreTypeKeySuffix)_level_\(self.localizedScoreLevelKeySuffix)"
+    }
+    
+    private var localizedScoreLevelKeySuffix: String {
+        switch self {
+            case .veryBad:
+                return "very_bad"
+            case .bad:
+                return "bad"
+            case .notGood:
+                return "low"
+            case .medium:
+                return "average"
+            case .great:
+                return "good"
+            case .veryGood:
+                return "very_good"
+            case .excellent:
+                return "excellent"
+        }
+    }
+}
+
+extension DKScoreType {
+    var localizedScoreTypeKeySuffix: String {
+        switch self {
+            case .safety:
+                return "safety"
+            case .ecoDriving:
+                return "ecodriving"
+            case .distraction:
+                return "distraction"
+            case .speeding:
+                return "speeding"
+        }
+    }
+}
---
+++ DriveKitDriverDataUI/MySynthesis/Components/ScoreLevelLegend/ScoreLevelLegendViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  ScoreLevelLegendViewModel.swift
+//  DriveKitDriverDataUI
+//
+//  Created by Frédéric Ruaudel on 03/03/2023.
+//  Copyright © 2023 DriveQuant. All rights reserved.
+//
+
+import DriveKitCoreModule
+import DriveKitCommonUI
+import Foundation
+
+public class ScoreLevelLegendViewModel {
+    public var scoreType: DKScoreType?
+    public var scoreLevelRowViewModels: [ScoreLevelLegendRowViewModel]?
+    
+    public init() {}
+    
+    public var legendTitle: String? {
+        guard let scoreType else { return nil }
+        return "dk_driverdata_\(scoreType.localizedScoreTypeKeySuffix)_score".dkDriverDataLocalized()
+    }
+    
+    public var legendDescription: String? {
+        guard let scoreType else { return nil }
+        return "dk_driverdata_mysynthesis_\(scoreType.localizedScoreTypeKeySuffix)_score_info".dkDriverDataLocalized()
+    }
+    
+    public func configure(
+        with scoreType: DKScoreType
+    ) {
+        self.scoreType = scoreType
+        scoreLevelRowViewModels = DKScoreTypeLevel.allCases.reversed().map { scoreTypeLevel in
+            let viewModel = ScoreLevelLegendRowViewModel()
+            viewModel.configure(with: scoreType, scoreTypeLevel: scoreTypeLevel)
+            return viewModel
+        }
+    }
+}

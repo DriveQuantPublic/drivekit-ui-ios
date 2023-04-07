a7b6eca71f24baa7becc7d277b9c069ac0a551d2
Frédéric Ruaudel <github@fredericruaudel.fr>
2023-04-07 11:57:03 +0200

    Reorganize folders and add files for TripDistance components

---
+++ DriveKitDriverDataUI/DrivingConditions/Components/TripDistanceCard/TripDistanceCardView.swift
@@ -0,0 +1,21 @@
+//
+//  TripDistanceCardView.swift
+//  DriveKitDriverDataUI
+//
+//  Created by Frédéric Ruaudel on 07/04/2023.
+//  Copyright © 2023 DriveQuant. All rights reserved.
+//
+
+import UIKit
+
+class TripDistanceCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DriveKitDriverDataUI/DrivingConditions/Components/TripDistanceCard/TripDistanceCardViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TripDistanceCardViewModel.swift
+//  DriveKitDriverDataUI
+//
+//  Created by Frédéric Ruaudel on 07/04/2023.
+//  Copyright © 2023 DriveQuant. All rights reserved.
+//
+
+import Foundation

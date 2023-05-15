adc201935dbe4811fe80fbea2c5bbde026fedc09
Frédéric Ruaudel <github@fredericruaudel.fr>
2023-05-15 16:10:07 +0200

    Create DKTripStopConfirmationViewModel

---
+++ DriveKitTripAnalysisUI/TripRecordingWidget/DKTripStopConfirmationDialog/DKTripStopConfirmationViewModel.swift
@@ -0,0 +1,67 @@
+//
+//  DKTripStopConfirmationViewModel.swift
+//  DriveKitTripAnalysisUI
+//
+//  Created by Frédéric Ruaudel on 15/05/2023.
+//  Copyright © 2023 DriveQuant. All rights reserved.
+//
+
+import CoreLocation
+import DriveKitCommonUI
+import DriveKitTripAnalysisModule
+import Foundation
+
+public class DKTripStopConfirmationViewModel {
+    
+    public var viewModelDidUpdate: (() -> Void)?
+    
+    public init() { }
+    
+    public var endTripTitle: String {
+        "dk_tripwidget_confirm_endtrip_title".dkTripAnalysisLocalized()
+    }
+    
+    public var endTripSubtitle: String {
+        "dk_tripwidget_confirm_endtrip_subtitle".dkTripAnalysisLocalized()
+    }
+    
+    public var continueTripTitle: String {
+        "dk_tripwidget_confirm_continuetrip_title".dkTripAnalysisLocalized()
+    }
+    
+    public var continueTripSubtitle: String {
+        "dk_tripwidget_confirm_continuetrip_subtitle".dkTripAnalysisLocalized()
+    }
+    
+    public var cancelTripTitle: String {
+        "dk_tripwidget_confirm_canceltrip_title".dkTripAnalysisLocalized()
+    }
+    
+    public var cancelTripSubtitle: String {
+        "dk_tripwidget_confirm_canceltrip_subtitle".dkTripAnalysisLocalized()
+    }
+    
+    public var disableRecordingConfirmationTitle: String {
+        "dk_tripwidget_disable_autostart_title".dkTripAnalysisLocalized()
+    }
+    
+    // swiftlint:disable no_magic_numbers
+    public var disableRecordingConfirmationTitlesAndDurations: [(String, Int)] {
+        [
+            (String(format: "10 %@", DKCommonLocalizable.minutePlural.text()), 10),
+            (String(format: "30 %@", DKCommonLocalizable.minutePlural.text()), 30),
+            (String(format: "1 %@", DKCommonLocalizable.hourSingular.text()), 60),
+            (String(format: "2 %@", DKCommonLocalizable.hourPlural.text()), 120),
+            (String(format: "4 %@", DKCommonLocalizable.hourPlural.text()), 240)
+        ]
+    }
+    // swiftlint:enable no_magic_numbers
+
+    public func endTripButtonTapped() {
+        DriveKitTripAnalysis.shared.stopTrip()
+    }
+    
+    public func disableRecordingConfirmationOptionSelected(duration: Int) {
+        DriveKitTripAnalysis.shared.temporaryDeactivateSDK(minutes: duration)
+    }
+}

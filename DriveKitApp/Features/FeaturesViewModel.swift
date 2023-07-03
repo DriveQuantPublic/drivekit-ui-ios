9e020f60076fe241617f5d5f45b4527080c3dba9
Frédéric Ruaudel <github@fredericruaudel.fr>
2023-07-03 11:10:54 +0200

    Add filter of driver profile feature when access is forbidden

--- DriveKitApp/Features/FeaturesViewModel.swift
+++ DriveKitApp/Features/FeaturesViewModel.swift
@@ -7,6 +7,7 @@
 //  Copyright © 2022 DriveQuant. All rights reserved.
 //
 
+import DriveKitDBTripAccessModule
 import Foundation
 
 class FeaturesViewModel {
@@ -24,5 +25,8 @@ class FeaturesViewModel {
         FeatureViewViewModel(type: .driverAchievement_badges),
         FeatureViewViewModel(type: .driverAchievement_streaks),
         FeatureViewViewModel(type: .tripAnalysis_workingHours)
-    ]
+    ].filter {
+        $0.type != .driverData_driver_profile ||
+        DriveKitAccess.shared.hasAccess(.driverProfile)
+    }
 }

1d25cf4fac7b8cbe788cedc6b85c61ab59fef5a7
Amine GAHBICHE <amine.gahbiche@drivequant.com>
2025-06-27 12:34:50 +0200

    Update DriveKit version

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/DriveQuantPublic/drivekit-sdk-spm.git",
-            from: "2.17.0"
+            from: "2.17.1"
         ),
         .package(
             url: "https://github.com/danielgindi/Charts.git",

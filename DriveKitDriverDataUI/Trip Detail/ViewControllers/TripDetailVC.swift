f22a1e98c274549314fc98504341320d80e53b94
Amine Gahbiche <amine.gahbiche@drivequant.com>
2026-07-17 16:09:55 +0200

    TripDetailsVC: remove deprecated imageEdgeInsets attribute and use UIButton.Configuration instead

--- DriveKitDriverDataUI/Trip Detail/ViewControllers/TripDetailVC.swift
+++ DriveKitDriverDataUI/Trip Detail/ViewControllers/TripDetailVC.swift
@@ -258,14 +258,22 @@ extension TripDetailVC {
     
     func setupTipButton() {
         if let trip = viewModel.trip, let advice = viewModel.displayMapItem?.getAdvice(trip: trip) {
-            tipButton.layer.borderColor = UIColor.black.cgColor
-            tipButton.layer.cornerRadius = tipButton.bounds.size.width / 2
-            tipButton.layer.masksToBounds = true
-            tipButton.backgroundColor = DKUIColors.secondaryColor.color
-            let image = advice.adviceImage()?.withRenderingMode(.alwaysTemplate)
-            tipButton.setImage(image, for: .normal)
-            tipButton.tintColor = .white
-            tipButton.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+            let imagePadding = 8.0
+            let imageWidth = tipButton.frame.size.width
+            var config = UIButton.Configuration.plain()
+            if let rawImage = advice.adviceImage() {
+                let targetWdith = imageWidth - 2*imagePadding
+                let targetSize = CGSize(width: targetWdith, height: targetWdith)
+                config.image = UIGraphicsImageRenderer(size: targetSize).image { _ in
+                    rawImage.draw(in: CGRect(origin: .zero, size: targetSize))
+                }.withRenderingMode(.alwaysTemplate)
+            }
+            config.background.backgroundColor = DKUIColors.secondaryColor.color
+            config.background.cornerRadius = imageWidth/2
+            config.contentInsets = .zero
+            config.baseForegroundColor = .white
+            tipButton.configuration = config
+            
             tipButton.isHidden = false
             self.mapContainer.bringSubviewToFront(tipButton)
             if showAdvice {
